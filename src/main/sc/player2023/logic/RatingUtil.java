f3ea069f45706b54b84c7475adb15821332b0a9e
goldos24 <ls2484xmails@gmail.com>
2022-10-09 22:41:40 +0200

    RatingUtil code enfancyment

--- src/main/sc/player2023/logic/RatingUtil.java
+++ src/main/sc/player2023/logic/RatingUtil.java
@@ -1,28 +1,18 @@
 package sc.player2023.logic;
 
 import sc.api.plugins.ITeam;
-import sc.api.plugins.Team;
 import sc.plugin2023.GameState;
 
 import javax.annotation.Nonnull;
-import java.util.Map;
-import static java.util.Map.entry;
+import java.util.stream.IntStream;
+
 
 public class RatingUtil {
-    private static final Map<ITeam, Integer> teamIndexMap = Map.ofEntries(
-            entry(Team.ONE, 0),
-            entry(Team.TWO, 1)
-    );
-
     public static int getCombinedPointsForTeam(@Nonnull GameState gameState, ITeam team) {
-        return gameState.getFishes()[teamIndexMap.get(team)];
+        return IntStream.of(gameState.getPointsForTeam(team)).sum();
     }
 
     public static boolean isTeamWinnerAfterGameEnd(GameState gameState, ITeam team) {
         return RatingUtil.getCombinedPointsForTeam(gameState, team) > RatingUtil.getCombinedPointsForTeam(gameState, team.opponent());
-    }
-
-    public static int prefixForCurrentTeam(@Nonnull GameState gameState) {
-        return gameState.getCurrentTeam() == Team.ONE ? 1 : -1;
     }
 }
