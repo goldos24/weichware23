952a430e273023617e94cfe7cbe20b03864ae26b
goldos24 <ls2484xmails@gmail.com>
2022-10-31 12:12:50 +0100

    Add move direction vectors

--- src/main/sc/player2023/logic/PossibleMoveIterator.java
+++ src/main/sc/player2023/logic/PossibleMoveIterator.java
@@ -1,6 +1,7 @@
 package sc.player2023.logic;
 
 import sc.api.plugins.ITeam;
+import sc.api.plugins.Vector;
 import sc.plugin2023.Move;
 
 import java.util.Iterator;
@@ -9,6 +10,19 @@ import java.util.Iterator;
 public class PossibleMoveIterator implements Iterator<Move> {
     BoardPeek board;
     ITeam team;
+
+    private static final int halfTileXChange = 1;
+    private static final int fullTileXChange = 2;
+    private static final int fullTileYChange = 1;
+
+    private static final Vector[] possibleMoveDirections = {
+            new Vector(-fullTileXChange, 0), // left
+            new Vector(-halfTileXChange, fullTileYChange), // top left
+            new Vector(halfTileXChange, fullTileYChange), // top right
+            new Vector(fullTileXChange, 0), // right
+            new Vector(halfTileXChange, -fullTileYChange), // bottom right
+            new Vector(-halfTileXChange, -fullTileYChange), // bottom left
+    };
 
     public PossibleMoveIterator(BoardPeek board, ITeam team) {
         this.board = board;
