1ed05e58c625b48265125594145f4901c1d1cc6c
goldos24 <ls2484xmails@gmail.com>
2022-11-02 17:43:33 +0100

    made ComparisonProgram.java do what it says

--- src/main/sc/player2023/comparison/ComparisonProgram.java
+++ src/main/sc/player2023/comparison/ComparisonProgram.java
@@ -4,9 +4,9 @@ import sc.player2023.logic.*;
 
 public class ComparisonProgram {
     public static void main(String[] args) {
-        Rater rater = new StupidRater();
+        Rater rater = new CompositeRater(new Rater[]{new WeightedRater(5, new StupidRater()), new PotentialFishRater()});
         Fighter mctsFighter = new Fighter(new PureMCTSMoveGetter(), rater);
-        Fighter pvsFighter = new Fighter(new StupidMoveGetter(), rater);
+        Fighter pvsFighter = new Fighter(new PVSMoveGetter(), rater);
         BattleResult result = Battle.run(mctsFighter, pvsFighter, new BattleData(1));
         System.out.println(result);
     }
