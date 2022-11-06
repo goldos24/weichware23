b69b85f823a0888c33c84b66cff01082ee23bb99
goldos24 <ls2484xmails@gmail.com>
2022-11-06 20:45:00 +0100

    add ReachableFishRater stub class

---
+++ src/main/sc/player2023/logic/rating/ReachableFishRater.java
@@ -0,0 +1,12 @@
+package sc.player2023.logic.rating;
+
+import sc.player2023.logic.ImmutableGameState;
+
+import javax.annotation.Nonnull;
+
+public class ReachableFishRater implements Rater {
+    @Override
+    public Rating rate(@Nonnull ImmutableGameState gameState) {
+        return null;
+    }
+}
