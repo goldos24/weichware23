4fa4421dc733ea411e30f747fc7e74596b3acf59
jarvarvarvis <jarvistrigo@gmail.com>
2022-11-06 14:38:05 +0100

    Fixed Selection unit test

--- src/test/java/sc/player2023/logic/SelectionTest.java
+++ src/test/java/sc/player2023/logic/SelectionTest.java
@@ -2,8 +2,11 @@ package sc.player2023.logic;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sc.player2023.logic.mcts.ImmutableMCTSTree;
 import sc.player2023.logic.mcts.ImmutableMCTSTreeNode;
 import sc.player2023.logic.mcts.Selection;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,18 +15,26 @@ public class SelectionTest {
     private Selection selection;
     private ImmutableGameState gameState;
 
+    private ImmutableMCTSTree tree;
+
     @BeforeEach
     void setUp() {
         ImmutableGameState gameState = GameStateFixture.createTestGameState();
         ImmutableMCTSTreeNode rootNode = new ImmutableMCTSTreeNode(gameState);
         this.gameState = gameState;
         this.selection = new Selection(rootNode);
+        this.tree = new ImmutableMCTSTree(this.gameState);
     }
 
     @Test
     void selectionReturnsInitialGameStateTest() {
-        ImmutableMCTSTreeNode completed = this.selection.complete();
-        ImmutableGameState completedGameState = completed.getGameState();
+        List<Integer> selectedNodeTrace = this.selection.complete();
+        assertEquals(selectedNodeTrace.size(), 0);
+
+        ImmutableMCTSTreeNode selectedNode = this.tree.trace(selectedNodeTrace);
+        assertNotNull(selectedNode);
+
+        ImmutableGameState completedGameState = selectedNode.getGameState();
         assertEquals(gameState, completedGameState);
     }
 }
