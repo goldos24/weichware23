4091075060ef7661f075d40b1e680cf6380dfa4a
jarvarvarvis <jarvistrigo@gmail.com>
2022-11-07 20:04:31 +0100

    Implemented all steps of MCTS in move getter, there are some issues with the node selection though

--- src/main/sc/player2023/logic/PureMCTSMoveGetter.java
+++ src/main/sc/player2023/logic/PureMCTSMoveGetter.java
@@ -11,20 +11,30 @@ import javax.annotation.Nonnull;
 import java.util.List;
 
 public class PureMCTSMoveGetter implements MoveGetter {
-    private final TimeMeasurer timer = new TimeMeasurer(1900);
+    private final TimeMeasurer timer = new TimeMeasurer(500);
 
     // TODO: find a more suitable exploration weight for this game
     public static final double EXPLORATION_WEIGHT = Math.sqrt(2);
-    public static final int EXPANSION_AMOUNT = 2;
+    public static final int EXPANSION_AMOUNT = 1;
 
     @Override
     public Move getBestMove(@Nonnull ImmutableGameState gameState, @Nonnull Rater rater, TimeMeasurer timeMeasurer) {
         timer.reset();
 
         ImmutableMCTSTree tree = new ImmutableMCTSTree(gameState);
+
+        System.out.println("MCTS begins");
+
+        long totalSelectionPathLength = 0;
+        long selections = 0;
+
         while (!timer.ranOutOfTime()) {
             Selection selection = tree.select();
             List<Integer> selectedPath = selection.complete();
+
+            totalSelectionPathLength += selectedPath.size();
+            selections++;
+
             Expansion expansion = tree.expand(selectedPath);
 
             // Dead end, no expansion is possible
@@ -32,9 +42,28 @@ public class PureMCTSMoveGetter implements MoveGetter {
                 continue;
             }
 
+            // The selected node had a terminal game state -> do nothing
             ImmutableMCTSTreeNode expandedNode = expansion.expandAndSimulate(EXPANSION_AMOUNT);
+
+            // If the selected path is empty, the expanded node becomes the new root node
+            if (selectedPath.isEmpty()) {
+                tree = new ImmutableMCTSTree(expandedNode);
+                continue;
+            }
+
+            // If the selected path is not empty, it must be inserted into the root node
+            ImmutableMCTSTreeNode rootNode = tree.getRootNode();
+            ImmutableMCTSTreeNode rootNodeWithExpandedNode = rootNode.withBackpropagatedChildAfterSteps(selectedPath, expandedNode);
+            tree = new ImmutableMCTSTree(rootNodeWithExpandedNode);
         }
 
-        return tree.bestMove();
+        ImmutableMCTSTreeNode treeRootNode = tree.getRootNode();
+        System.out.printf("MCTS completed, tree size: %d\n", treeRootNode.calculateSize());
+        System.out.printf("Root node visits: %d\n", tree.getRootNode().getStatistics().visits());
+        System.out.printf("Average selection path length: %f\n", (double)totalSelectionPathLength / selections);
+
+        Move move = tree.bestMove();
+        System.out.printf("Selected move: %s\n", move);
+        return move;
     }
 }
