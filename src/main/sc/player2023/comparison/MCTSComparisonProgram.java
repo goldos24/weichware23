9d6576c079eba1f37daeb86e8881f7fbc2ae2bc7
jarvarvarvis <jarvistrigo@gmail.com>
2022-11-13 00:01:56 +0100

    Added comparison between AllMovesInitialiser and BestRatedMovesInitialiser in MCTS comparison program - BestRatedMovesInitialiser proved to be better

--- src/main/sc/player2023/comparison/MCTSComparisonProgram.java
+++ src/main/sc/player2023/comparison/MCTSComparisonProgram.java
@@ -6,8 +6,8 @@ import sc.player2023.logic.mcts.NodeEvaluator;
 import sc.player2023.logic.mcts.NodeSelector;
 import sc.player2023.logic.mcts.RootChildrenInitialiser;
 import sc.player2023.logic.mcts.evaluators.*;
-import sc.player2023.logic.mcts.initialisers.BestRatedMovesInitialiser;
-import sc.player2023.logic.mcts.selectors.BasicEvaluatorSelector;
+import sc.player2023.logic.mcts.initialisers.*;
+import sc.player2023.logic.mcts.selectors.*;
 import sc.player2023.logic.rating.*;
 
 public class MCTSComparisonProgram {
@@ -15,13 +15,13 @@ public class MCTSComparisonProgram {
         Rater rater = Logic.createCombinedRater();
         NodeEvaluator scoreDiffWeightedEvaluator = new ScoreDiffWeightedUCTEvaluator(MCTSMoveGetter.THEORETICAL_EXPLORATION_WEIGHT);
         NodeSelector selector = new BasicEvaluatorSelector(scoreDiffWeightedEvaluator);
-        RootChildrenInitialiser initialiser = new BestRatedMovesInitialiser(rater, 15);
-        RootChildrenInitialiser otherInitialiser = new BestRatedMovesInitialiser(rater, 10);
+        RootChildrenInitialiser initialiser = new AllMovesInitialiser();
+        RootChildrenInitialiser otherInitialiser = new BestRatedMovesInitialiser(rater, 8);
 
         Fighter mctsFighter = new Fighter(new MCTSMoveGetter(selector, initialiser, 3), rater);
         Fighter otherMctsFighter = new Fighter(new MCTSMoveGetter(selector, otherInitialiser, 8), rater);
 
-        BattleResult result = Battle.run(mctsFighter, otherMctsFighter, new BattleData(1));
+        BattleResult result = Battle.run(mctsFighter, otherMctsFighter, new BattleData(3));
         System.out.println(result);
     }
 }
