94349d25a4640427dec48dae3b555c228dfe4621
goldos24 <ls2484xmails@gmail.com>
2022-12-04 22:46:09 +0100

    fix compiler error

--- src/test/java/sc/player2023/logic/movetests/NoSelfCutOffTest.java
+++ src/test/java/sc/player2023/logic/movetests/NoSelfCutOffTest.java
@@ -10,13 +10,14 @@ import sc.player2023.logic.board.BoardPeek;
 import sc.player2023.logic.gameState.ImmutableGameState;
 import sc.player2023.logic.pvs.PVSMoveGetter;
 import sc.player2023.logic.rating.Rater;
+import sc.player2023.logic.score.GameScore;
 import sc.plugin2023.Move;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class NoSelfCutOffTest {
     Rater rater = Logic.createCombinedRater();
 
-    Move getMoveForDepth(BoardPeek situation, Team team, Integer[] teamPointsMap, int depth) {
+    Move getMoveForDepth(BoardPeek situation, Team team, GameScore teamPointsMap, int depth) {
         return PVSMoveGetter.getBestMoveForDepth(
                 new ImmutableGameState(teamPointsMap, situation, team),
                 rater,
@@ -41,8 +42,7 @@ public class NoSelfCutOffTest {
                 new Move(new Coordinates(14, 4), new Coordinates(15, 5)),
                 new Move(new Coordinates(7, 5), new Coordinates(9, 5))
         };
-        ImmutableGameState gameState = new ImmutableGameState(new Integer[]{0, 0}, BoardParser.boardFromString(situation), Team.ONE);
-        Move got = getMoveForDepth(BoardParser.boardFromString(situation), Team.ONE, new Integer[]{0, 0}, 0);
+        Move got = getMoveForDepth(BoardParser.boardFromString(situation), Team.ONE, new GameScore(0,0), 0);
         for(Move unexpected : unexpectedMoves) {
             assertNotEquals(unexpected, got);
         }
