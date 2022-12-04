8108b9b7564ed004dcf3ee2dd994e968401d4477
goldos24 <ls2484xmails@gmail.com>
2022-12-04 20:42:15 +0100

    add TeamRater

--- src/main/sc/player2023/logic/rating/RatingUtil.java
+++ src/main/sc/player2023/logic/rating/RatingUtil.java
@@ -1,6 +1,7 @@
 package sc.player2023.logic.rating;
 
 import sc.api.plugins.ITeam;
+import sc.player2023.logic.gameState.ImmutableGameState;
 import sc.plugin2023.GameState;
 
 import javax.annotation.Nonnull;
@@ -10,5 +11,11 @@ import java.util.stream.IntStream;
 public class RatingUtil {
     public static int getCombinedPointsForTeam(@Nonnull GameState gameState, ITeam team) {
         return IntStream.of(gameState.getPointsForTeam(team)).sum();
+    }
+
+    public static Rating combineTeamRatings(@Nonnull ImmutableGameState gameState, @Nonnull TeamRater teamRater) {
+        var ownTeam = gameState.getCurrentTeam();
+        var opponentTeam = ownTeam.opponent();
+        return teamRater.rateForTeam(gameState, ownTeam).subtract(teamRater.rateForTeam(gameState, opponentTeam));
     }
 }
