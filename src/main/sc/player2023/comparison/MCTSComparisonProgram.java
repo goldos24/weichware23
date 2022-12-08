9dcaa58b5d5ea841f278e39e576701965cb6a780
jarvarvarvis <jarvistrigo@gmail.com>
2022-12-08 14:54:20 +0100

    Cleaned up MCTSComparisonProgram.java

--- src/main/sc/player2023/comparison/MCTSComparisonProgram.java
+++ src/main/sc/player2023/comparison/MCTSComparisonProgram.java
@@ -1,13 +1,15 @@
 package sc.player2023.comparison;
 
-import sc.player2023.logic.Logic;
 import sc.player2023.logic.MCTSMoveGetter;
-import sc.player2023.logic.mcts.*;
-import sc.player2023.logic.mcts.evaluators.*;
+import sc.player2023.logic.mcts.NodeEvaluator;
+import sc.player2023.logic.mcts.NodeExpander;
+import sc.player2023.logic.mcts.NodeSelector;
+import sc.player2023.logic.mcts.RootChildrenInitialiser;
+import sc.player2023.logic.mcts.evaluators.ScoreDiffWeightedUCTEvaluator;
 import sc.player2023.logic.mcts.expanders.FullNodeExpander;
 import sc.player2023.logic.mcts.expanders.SimulatingNodeExpansionProvider;
-import sc.player2023.logic.mcts.initialisers.*;
-import sc.player2023.logic.mcts.selectors.*;
+import sc.player2023.logic.mcts.initialisers.AllMovesInitialiser;
+import sc.player2023.logic.mcts.selectors.BasicEvaluatorSelector;
 import sc.player2023.logic.pvs.PVSMoveGetter;
 import sc.player2023.logic.rating.*;
 
@@ -23,7 +25,6 @@ public class MCTSComparisonProgram {
     }
 
     public static void main(String[] args) {
-        Rater rater = Logic.createCombinedRater();
         NodeEvaluator scoreDiffWeightedEvaluator = new ScoreDiffWeightedUCTEvaluator(MCTSMoveGetter.THEORETICAL_EXPLORATION_WEIGHT);
 
         NodeSelector selector = new BasicEvaluatorSelector(scoreDiffWeightedEvaluator);
