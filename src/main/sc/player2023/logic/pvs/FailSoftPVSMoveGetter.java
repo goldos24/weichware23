6a51a846db85db62ea7cbdf1a02e7a761440bf61
goldos24 <ls2484xmails@gmail.com>
2022-12-22 12:36:48 +0100

    make fail-soft pvs function public + add reduced window unit test

--- src/main/sc/player2023/logic/pvs/FailSoftPVSMoveGetter.java
+++ src/main/sc/player2023/logic/pvs/FailSoftPVSMoveGetter.java
@@ -39,7 +39,7 @@ public class FailSoftPVSMoveGetter implements MoveGetter {
     }
 
 
-    private static RatingWithMove pvs(@Nonnull ImmutableGameState gameState, int depth, AlphaBeta alphaBeta,
+    public static RatingWithMove pvs(@Nonnull ImmutableGameState gameState, int depth, AlphaBeta alphaBeta,
                               @Nonnull Rater rater, @Nonnull TimeMeasurer timeMeasurer,
                               TransPositionTable transPositionTable) {
         if(transPositionTable.hasGameState(gameState)) {
@@ -138,7 +138,8 @@ public class FailSoftPVSMoveGetter implements MoveGetter {
     public static Move getBestMoveForDepth(@Nonnull ImmutableGameState gameState, @Nonnull Rater rater,
                                            TimeMeasurer timeMeasurer, int depth,
                                            TransPositionTable transPositionTable) {
-        return pvs(gameState, depth, new AlphaBeta(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY), rater, timeMeasurer, transPositionTable).move();
+        AlphaBeta alphaBeta = new AlphaBeta(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+        return pvs(gameState, depth, alphaBeta, rater, timeMeasurer, transPositionTable).move();
     }
 
 }
--- src/test/java/sc/player2023/logic/movetests/OpponentCutOffTest.java
+++ src/test/java/sc/player2023/logic/movetests/OpponentCutOffTest.java
@@ -9,7 +9,9 @@ import sc.player2023.logic.TimeMeasurerFixture;
 import sc.player2023.logic.board.BoardParser;
 import sc.player2023.logic.gameState.ImmutableGameState;
 import sc.player2023.logic.pvs.FailSoftPVSMoveGetter;
+import sc.player2023.logic.rating.AlphaBeta;
 import sc.player2023.logic.rating.Rater;
+import sc.player2023.logic.rating.RatingWithMove;
 import sc.player2023.logic.score.GameScore;
 import sc.player2023.logic.transpositiontable.SimpleTransPositionTableFactory;
 import sc.player2023.logic.transpositiontable.TransPositionTableFactory;
@@ -18,8 +20,8 @@ import sc.plugin2023.Move;
 public class OpponentCutOffTest {
     TransPositionTableFactory transPositionTableFactory = new SimpleTransPositionTableFactory();
 
-    @Test void lateGameHugeCutOff() {
-        String situation = """
+
+    String LATE_GAME_HUGE_CUTOFF_SITUATION = """
                 4 = G 3 P     -\s
                        3   = -  \s
                   = G -   -   =\s
@@ -29,11 +31,28 @@ public class OpponentCutOffTest {
                                \s
                  -       3 4 G  \s
                 """;
-        Move expectedMove = new Move(new Coordinates(8, 0), new Coordinates(5, 3));
+    Move LATE_GAME_HUGE_CUTOFF_EXPECTED_MOVE = new Move(new Coordinates(8, 0), new Coordinates(5, 3));
+
+    int LATE_GAME_HUGE_CUTOFF_DEPTH = 5;
+
+    @Test
+    void lateGameHugeCutOff() {
         Rater rater = Logic.createCombinedRater();
-        ImmutableGameState gameState = new ImmutableGameState(new GameScore(3, 0), BoardParser.boardFromString(situation), Team.TWO);
-        Move got = FailSoftPVSMoveGetter.getBestMoveForDepth(gameState, rater, TimeMeasurerFixture.createAlreadyRunningInfiniteTimeMeasurer(), 5,
+        ImmutableGameState gameState = new ImmutableGameState(new GameScore(3, 0), BoardParser.boardFromString(LATE_GAME_HUGE_CUTOFF_SITUATION), Team.TWO);
+        Move got = FailSoftPVSMoveGetter.getBestMoveForDepth(gameState, rater, TimeMeasurerFixture.createAlreadyRunningInfiniteTimeMeasurer(), LATE_GAME_HUGE_CUTOFF_DEPTH,
                 transPositionTableFactory.createTransPositionTableFromDepth(5));
-        assertEquals(expectedMove, got);
+        assertEquals(LATE_GAME_HUGE_CUTOFF_EXPECTED_MOVE, got);
+    }
+    @Test
+    void reducedWindowLateGameHugeCutOff() {
+        Rater rater = Logic.createCombinedRater();
+        ImmutableGameState gameState = new ImmutableGameState(new GameScore(3, 0), BoardParser.boardFromString(LATE_GAME_HUGE_CUTOFF_SITUATION), Team.TWO);
+        AlphaBeta alphaBeta = new AlphaBeta(-210, -208);
+        RatingWithMove ratingWithMove = FailSoftPVSMoveGetter.pvs(gameState, LATE_GAME_HUGE_CUTOFF_DEPTH, alphaBeta, rater,
+                TimeMeasurerFixture.createAlreadyRunningInfiniteTimeMeasurer(),
+                transPositionTableFactory.createTransPositionTableFromDepth(LATE_GAME_HUGE_CUTOFF_DEPTH));
+        Move got = ratingWithMove.move();
+        System.out.println(ratingWithMove.rating());
+        assertEquals(LATE_GAME_HUGE_CUTOFF_EXPECTED_MOVE, got);
     }
 }
