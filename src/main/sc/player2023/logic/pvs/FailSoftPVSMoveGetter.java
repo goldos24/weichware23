f72541e8488eb028c2f50db0371bde8796df3fcb
goldos24 <ls2484xmails@gmail.com>
2023-02-16 15:43:15 +0100

    remove warnings

--- src/main/sc/player2023/logic/pvs/FailSoftPVSMoveGetter.java
+++ src/main/sc/player2023/logic/pvs/FailSoftPVSMoveGetter.java
@@ -1,17 +1,10 @@
 package sc.player2023.logic.pvs;
 
-import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sc.player2023.logic.GameRuleLogic;
-import sc.player2023.logic.MoveGetter;
-import sc.player2023.logic.TimeMeasurer;
 import sc.player2023.logic.gameState.ImmutableGameState;
-import sc.player2023.logic.move.PossibleMoveGenerator;
 import sc.player2023.logic.move.PossibleMoveStreamFactory;
-import sc.player2023.logic.rating.*;
 import sc.player2023.logic.transpositiontable.SmartTransPositionTableFactory;
-import sc.player2023.logic.transpositiontable.TransPositionTable;
 import sc.player2023.logic.transpositiontable.TransPositionTableFactory;
 import sc.plugin2023.Move;
 
@@ -19,11 +12,6 @@ import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import static sc.player2023.logic.GameRuleLogic.getPossibleMoves;
-import static sc.player2023.logic.GameRuleLogic.withMovePerformed;
-import static sc.player2023.logic.pvs.MoveGetterUtil.getRatingFactorForNextMove;
-import static sc.player2023.logic.rating.RatingUtil.isInSearchWindow;
 
 public class FailSoftPVSMoveGetter implements IterativeDeepeningAlphaBetaMoveGetter {
 
