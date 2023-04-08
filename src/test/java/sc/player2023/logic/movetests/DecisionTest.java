07f298682634ac2d86dd3f18569be850f30d6cf6
goldos24 <ls2484xmails@gmail.com>
2023-04-09 00:03:40 +0200

    remove stupid test

--- src/test/java/sc/player2023/logic/movetests/DecisionTest.java
+++ src/test/java/sc/player2023/logic/movetests/DecisionTest.java
@@ -39,28 +39,4 @@ public class DecisionTest {
         Move move = moveGetter.getBestMove(gameState, rater, timeMeasurer);
         assertEquals(new Move(new Coordinates(10, 4), new Coordinates(11, 5)), move);
     }
-
-    @Test
-    void twoFishAndCutOffOrThreeFishOrSomethingElseTest() {
-        BoardPeek testBoard = BoardParser.boardFromString("""
-                3 3 G G G   - -\s
-                 3   P P P     -\s
-                3 3         P  \s
-                 3 3 3 3   =   -\s
-                3 3 3 3   G   -\s
-                 3 3 3 4   -   -\s
-                4 4 4 4   =   -\s
-                 4 4 4 4     - -\s
-                """);
-        Team team = Team.ONE;
-        GameScore score = new GameScore(0, 0);
-        FailSoftPVSMoveGetter moveGetter = new FailSoftPVSMoveGetter();
-        TimeMeasurer timeMeasurer = TimeMeasurerFixture.createAlreadyRunningInfiniteTimeMeasurer();
-        ImmutableGameState gameState = new ImmutableGameState(score, testBoard, team);
-        Rater rater = Logic.createNewCombinedRater();
-        var transFactory = new SimpleTransPositionTableFactory();
-        int depth = 3;
-        Move move = moveGetter.getBestMoveForDepth(gameState, rater, timeMeasurer, depth, transFactory.createTransPositionTableFromDepth(depth));
-        assertEquals(new Move(new Coordinates(10, 4), new Coordinates(11, 5)), move);
-    }
 }
