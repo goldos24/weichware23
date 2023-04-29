b4e09c38fbc9d548a5ac3d6f87fa2c7d92df2f4a
jarvarvarvis <jarvistrigo@gmail.com>
2023-04-29 22:29:43 +0200

    Remove NoRegularizer.java, add Regularizer.NONE

--- src/main/sc/player2023/logic/nnet/Regularizer.java
+++ src/main/sc/player2023/logic/nnet/Regularizer.java
@@ -4,5 +4,7 @@ import sc.player2023.logic.nnet.math.Matrix;
 
 public interface Regularizer {
 
+    Regularizer NONE = (weights, delta) -> delta;
+
     Matrix regularize(Matrix weights, Matrix delta);
 }
